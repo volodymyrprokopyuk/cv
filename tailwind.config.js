71dbc2f296bcea2e16321d4e89684816323b5cce
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2024-07-11 15:30:05 +0200

    fix: Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "details.html"],
+  content: ["index.html", "details/index.html"],
   theme: {
     extend: {},
   },
